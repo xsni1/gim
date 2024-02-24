e98a1c1c02d0cc8a6c15af7818720b9482e5c1b0
xsni1 <thesniboy@gmail.com>
2024-02-24 22:53:32 +0100

    Cursor stays in correct place. Many bugs

--- main.go
+++ main.go
@@ -8,8 +8,23 @@ import (
 	"golang.org/x/term"
 )
 
+type Position struct {
+	x int
+	y int
+}
+
+// TODO: Terminal physical lines != text lines - this is causing LOTS of bugs
+// TODO: When text goes to the next line old text gets overwritten
+// TODO: \t not interpreted correctly
+
 func main() {
 	lines := [][]byte{}
+	insertMode := false
+	cursorPosition := Position{
+		x: 1,
+		y: 1,
+	}
+
 	// we have to restore it, otherwise terminal stays in raw mode
 	prevState, _ := term.MakeRaw(0)
 
@@ -21,18 +36,20 @@ func main() {
 
 	// alternate xterm screen
 	fmt.Print("\u001B[?1049h")
-	insertMode := false
 
 	// move to the top
 	fmt.Print("\033[1;1H")
 
 	for _, line := range lines {
 		fmt.Print(string(line))
+
+        // go down
 		fmt.Print("\033[1B")
 		// move to the first column
-        fmt.Print("\033[1G")
-		// fmt.Print("\033[1;1H")
+		fmt.Print("\033[1G")
 	}
+
+	fmt.Print("\033[1;1H")
 
 	defer func() {
 		term.Restore(0, prevState)
@@ -40,6 +57,7 @@ func main() {
 	}()
 
 	for {
+		// TODO: make it right
 		in := make([]byte, 10)
 		os.Stdin.Read(in)
 
@@ -49,6 +67,19 @@ func main() {
 
 		if insertMode {
 			fmt.Print(string(in[0]))
+			lines[cursorPosition.y-1] = append(lines[cursorPosition.y-1][:cursorPosition.x], lines[cursorPosition.y-1][cursorPosition.x-1:]...)
+			lines[cursorPosition.y-1][cursorPosition.x-1] = in[0]
+            cursorPosition.x++
+
+			// erase entire line
+			fmt.Print("\033[2K")
+			// move to the first column
+			fmt.Print("\033[1G")
+
+			fmt.Print(string(lines[cursorPosition.y-1]))
+
+			fmt.Printf("\033[%d;%dH", cursorPosition.y, cursorPosition.x)
+
 			continue
 		}
 
@@ -58,18 +89,22 @@ func main() {
 
 		if in[0] == 'j' {
 			os.Stdout.Write([]byte("\033[1B"))
+			cursorPosition.y++
 		}
 
 		if in[0] == 'k' {
 			os.Stdout.Write([]byte("\033[1A"))
+			cursorPosition.y--
 		}
 
 		if in[0] == 'l' {
 			os.Stdout.Write([]byte("\033[1C"))
+			cursorPosition.x++
 		}
 
 		if in[0] == 'h' {
 			os.Stdout.Write([]byte("\033[1D"))
+			cursorPosition.x--
 		}
 
 		if in[0] == 'i' {
@@ -79,5 +114,3 @@ func main() {
 	}
 }
 
-// przeczytac line by line caly plik - rozdzielajac po \n albo \r\n
-// printowac kazda linie jako osobna linie w terminalu
